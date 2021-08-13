b7a42bfd16a2ec33d858087da6763710af5df4c5
nanggi4 <nkhun1006@gmail.com>
2021-08-14 00:40:44 +0900

    tistory blog update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, {css, ThemeProvider } from 'styled-components';
 
-function App() {
+const theme = {
+  primary: "blue",
+  secondary: "red",
+}
+
+// 기본적인 사용방법은 아래와 같이 원하는 const COMPONENT_NAME = styled.HTML_TAG_NAME`css`; 라고 보면된다.
+// COMPONENT_NAME에는 이후에 사용할 컴포넌트의 이름이 들어가고
+// HTML_TAG_NAME에는 해당 컴포넌트가 가진 html tag 속성이 들어간다 (ex button, input, h1, h2, div, span)
+// 마지막으로 `` 백틱 태그안에 해당 컴포넌트가 가진 css 속성을 넣어주면 된다.
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 15px;
+
+  & input {
+    width: auto;
+  }
+
+  @media (max-width: 720px) {
+    & input {
+      width: 100%;
+    }
+  }
+
+`;
+
+const HeadingComponent = styled.h1`
+  font-family: sans-serif;
+`;
+
+const Input = styled.input`
+  padding: 5px;
+  border-radius: 4px;
+  margin: 10px;
+`;
+
+const Button = styled.button`
+  color: ${(props) => (props.isValid ? "white" : "red")};
+  background: ${(props) => (props.isValid ? "green" : "yellow")};
+  padding: 5px;
+
+  ${(props) =>
+    props.isValid 
+    ? css`
+      color: white;
+      background: green;
+    `
+    : css`
+      color: red;
+      background: yellow;
+    `
+  }
+
+  &:hover {
+    color: red;
+    transition: .2s all;
+  }
+`;
+
+// 그리고 나서 원하는 위치에 해당 컴포넌트를 덮어 씌어주면 된다.
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <HeadingComponent>Styled Components</HeadingComponent>
+        <Input placeholder="username" className={true ? 'valid' : 'invalid'}/>
+        <Input placeholder="password" />
+        <Button isValid={true}>Login</Button>
+      </Container>
+    </ThemeProvider>
   );
 }
 
